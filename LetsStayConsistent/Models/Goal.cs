9a848de3e3ed66d392f1692fc8688dec6ed2ebe3
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-05-10 16:21:04 -0500

    Add intial models

---
+++ LetsStayConsistent/Models/Goal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LetsStayConsistent.Models
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DaysToComplete { get; set; }
+
+        [Required]
+        public string Reward { get; set; }
+
+        List<GoalLog> GoalLogs { get; set; }
+    }
+}
---
+++ LetsStayConsistent/Models/GoalLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LetsStayConsistent.Models
+{
+    public class GoalLog
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool WasCompleted { get; set; }
+
+        public Goal Goal { get; set; }
+
+        public int GoalId { get; set; }
+    }
+}
