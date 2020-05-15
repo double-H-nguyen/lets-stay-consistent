a21c8460d2577cd1f2336f10f26b4615ca9511fa
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-05-14 19:40:17 -0500

    Added display names for Goal and GoalLog model

--- LetsStayConsistent/Models/GoalLog.cs
+++ LetsStayConsistent/Models/GoalLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,18 @@ namespace LetsStayConsistent.Models
 {
     public class GoalLog
     {
+        [Display(Name = "Id")]
         public int Id { get; set; }
 
+        [Display(Name = "Date logged")]
         public DateTime Date { get; set; }
 
+        [Display(Name = "Was the goal accomplished on this day?")]
         public bool WasCompleted { get; set; }
 
         public Goal Goal { get; set; }
 
+        [Display(Name = "Goal's id")]
         public int GoalId { get; set; }
     }
 }
