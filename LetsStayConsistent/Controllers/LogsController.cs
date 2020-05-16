f25e0e20452fac0dfaf17597f3c32de0ee0097ab
Henry Nguyen <nguyenhenryh1996@gmail.com>
2020-05-15 19:14:47 -0500

    Created form to add goal logs

--- LetsStayConsistent/Controllers/LogsController.cs
+++ LetsStayConsistent/Controllers/LogsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LetsStayConsistent.Models;
+using LetsStayConsistent.Utilities;
 using LetsStayConsistent.ViewModels;
 
 namespace LetsStayConsistent.Controllers
@@ -32,6 +33,43 @@ namespace LetsStayConsistent.Controllers
             };
 
             return View(model);
+        }
+
+        public ActionResult Add()
+        {
+            var model = new LogsAddViewModel
+            {
+                Goals = LogsUtility.GetGoalDropdown()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(LogsAddViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var newModel = new LogsAddViewModel
+                {
+                    Goals = LogsUtility.GetGoalDropdown(),
+                    GoalLog = model.GoalLog
+                };
+
+                return View("Add", newModel);
+            }
+
+            var log = new GoalLog
+            {
+                Date = model.GoalLog.Date,
+                WasCompleted = model.GoalLog.WasCompleted,
+                GoalId = model.GoalId
+            };
+
+            _context.GoalLogs.Add(log);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Goal");
         }
     }
 }
--- cwf-check-ins.md
+++ cwf-check-ins.md
@@ -26,6 +26,7 @@ This is a log of what I posted to `#check-ins` channel on the CWF's Discord.
 * Add labels attribute for models
 * Create initial goal's details view
 * Create initial logs view
+* Create form to add goal logs
 
 ## Week 3 (5/18/20 - 5/24/20)
 
